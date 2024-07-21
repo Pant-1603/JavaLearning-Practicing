50b8cf27d7dffbff03263f0577997376ee6b484b
Anurag Pant <pantanurag10@gmail.com>
2024-07-21 20:03:55 +0530

    Create MultiplicationTable.java

---
+++ MultiplicationTable.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+Scanner scanner= new Scanner(System.in);
+
+System.out.print("Enter a number: ");
+
+int num= scanner.nextInt();
+
+System.out.println("Multiplication table of " + num + " is: ");
+
+int i= 1;
+
+while(i<=10) {
+    System.out.println(num + " * " + i + " = " + (num*i));
+    i++;
+}
+scanner.close();
+}
+
+}

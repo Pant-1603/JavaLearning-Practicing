b9931b9eb5438baeada2c46dfdd9bfa780a720a0
Anurag Pant <pantanurag10@gmail.com>
2024-07-21 20:11:31 +0530

    Create PatternPrintNumbers.java

---
+++ PatternPrintNumbers.java
@@ -0,0 +1,14 @@
+public class PatternPrintNumbers {
+public static void main(String[] args) {
+//Outer loop for printing rows
+
+for(int i=1; i<=5; i++) {
+//Inner loop for printing columns
+    for(int j=1; j<=5; j++) {
+        System.out.print(i);
+    }
+    
+    System.out.println();
+}
+}
+}
